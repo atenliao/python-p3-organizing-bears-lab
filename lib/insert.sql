45249e70dac5c38c6a06f9df9eb944553912d008
zhongming <atenliao@gmail.com>
2023-08-16 12:29:29 -0700

    complete SQL statement

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr.Chocolate', 39, 'M','Red','soft',1),
+('Rowdy',20,'M','black','funny',0),
+('Tabitha',31,'F','white','cool',1),
+('Sergeant Brown',40,'M','brown','pretty',1),
+('Melissa',15,'F','blue','cute',1),
+('Grich',23,'M','green','nice',0),
+('Wendy',18,'F','yellow','beauty',1),
+(NULL,21,'M','blue','naive',1);
+
